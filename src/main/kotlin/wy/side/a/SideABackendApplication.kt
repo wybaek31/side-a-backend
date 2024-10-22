965501f2eb18effe99fe7958bf06014b60ba5cf8
wybaek31 <wybaek31@gmail.com>
2024-10-22 11:15:45 +0900

    - 프로젝트 생성

---
+++ src/main/kotlin/wy/side/a/SideABackendApplication.kt
@@ -0,0 +1,11 @@
+package wy.side.a
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SideABackendApplication
+
+fun main(args: Array<String>) {
+    runApplication<SideABackendApplication>(*args)
+}
---
+++ src/test/kotlin/wy/side/a/SideABackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package wy.side.a
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SideABackendApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
